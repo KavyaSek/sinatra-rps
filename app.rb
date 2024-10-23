f4a8381e703b98d87a004ba10ccbe359035e64cf
KavyaSek <ksekar@chicagobooth.edu>
2024-10-23 00:45:49 +0000

    AA

--- app.rb
+++ app.rb
@@ -5,3 +5,17 @@ get("/") do
   erb(:mainpage)
 end
 
+get("/rock") do
+  
+array = ["rock","paper","scissors"]
+@they = array.sample
+if @they == "rock"
+  @outcome = "tied!"
+elsif @they == "paper"
+  @outcome = "lost!"
+else
+  @outcome = "won!"
+end
+  
+  erb(:rock)
+end
